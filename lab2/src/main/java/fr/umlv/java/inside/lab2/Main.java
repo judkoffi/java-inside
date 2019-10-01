1d67ac2f9f7337dac06333ac20f5a857cf00c0b6
Judicael KOFFI <judkoffi@gmail.com>
2019-10-01 10:33:03 +0200

    add toJSON method

--- lab2/src/main/java/fr/umlv/java/inside/lab2/Main.java
+++ lab2/src/main/java/fr/umlv/java/inside/lab2/Main.java
@@ -1,5 +1,10 @@
 package fr.umlv.java.inside.lab2;
 
+import static java.util.stream.Collectors.joining;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Arrays;
 
 public class Main {
@@ -8,19 +13,36 @@ public class Main {
 		return Character.toLowerCase(name.charAt(3)) + name.substring(4);
 	}
 
+	private static String buildJSONLine(Object object, Method method) {
+		var property = propertyName(method.getName());
+		try {
+			var value = method.invoke(object);
+			return "\"" + property + "\"" + ": " + "\"" + value + "\"";
+		} catch (IllegalAccessException e) {
+			throw new IllegalStateException(e);
+		} catch (InvocationTargetException e) {
+			var cause = e.getCause();
+			if (cause instanceof RuntimeException) {
+				throw (RuntimeException) cause;
+			}
+			if (cause instanceof Error) {
+				throw (Error) cause;
+			}
+			throw new UndeclaredThrowableException(cause);
+		}
+	}
+
 	public static String toJSON(Object object) {
+
 		var methods = object.getClass().getMethods();
 
-		var stream = Arrays.stream(methods);
-		
-		stream.forEach((method) -> System.out.println(propertyName(method.getName())));
-
-		return "";
+		return Arrays.stream(methods).filter((method) -> method.getName().startsWith("get"))
+				.map((method) -> buildJSONLine(object, method)).collect(joining(",", " {", "}"));
 	}
 
 	public static void main(String[] args) {
 		var person = new Person("John", "Doe");
-		toJSON(person);
+		System.out.println(toJSON(person));
 	}
 
 }
