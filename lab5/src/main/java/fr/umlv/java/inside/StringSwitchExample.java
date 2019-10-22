ce19f10a400b1fca289594bf61896b23f59d91ec
Judicael KOFFI <judkoffi@gmail.com>
2019-10-22 12:14:42 +0200

    implement stringSwitch3

--- lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
+++ lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
@@ -8,53 +8,107 @@ import static java.lang.invoke.MethodType.methodType;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.MutableCallSite;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.util.List;
 import java.util.Objects;
 
 public class StringSwitchExample {
-    private static final MethodHandle STRING_EQUALS;
-    static {
-	var lookup = MethodHandles.lookup();
+	private static final MethodHandle STRING_EQUALS;
+	static {
+		var lookup = MethodHandles.lookup();
 
-	try {
-	    STRING_EQUALS = lookup.findVirtual(String.class, "equals", methodType(boolean.class, Object.class));
-	} catch (NoSuchMethodException | IllegalAccessException e) {
-	    throw new AssertionError(e);
-	}
-    };
-
-    @SuppressWarnings("preview")
-    public static int stringSwitch(String str) {
-	Objects.requireNonNull(str);
-	return switch (str) {
-	case "foo" -> 0;
-	case "bar" -> 1;
-	case "bazz" -> 2;
-	default -> -1;
+		try {
+			STRING_EQUALS = lookup.findVirtual(String.class, "equals",
+					methodType(boolean.class, Object.class));
+		} catch (NoSuchMethodException | IllegalAccessException e) {
+			throw new AssertionError(e);
+		}
 	};
 
-    }
+	@SuppressWarnings("preview")
+	public static int stringSwitch(String str) {
+		Objects.requireNonNull(str);
+		return switch (str) {
+			case "foo" -> 0;
+			case "bar" -> 1;
+			case "bazz" -> 2;
+			default -> -1;
+		};
 
-    public static int stringSwitch2(String str) {
-	Objects.requireNonNull(str);
-	try {
-	    var mh = createMHFromStrings2("foo", "bar", "bazz");
-	    return (int) mh.invokeExact(str);
-	} catch (RuntimeException | Error e) {
-	    throw e;
-	} catch (Throwable t) {
-	    throw new UndeclaredThrowableException(t);
-	}
-    }
-
-    private static MethodHandle createMHFromStrings2(String... strings) throws Throwable {
-	var mh = dropArguments(constant(int.class, -1), 0, String.class);
-
-	for (int i = 0; i < strings.length; i++) {
-	    mh = guardWithTest(insertArguments(STRING_EQUALS, 1, strings[i]),
-		    dropArguments(constant(int.class, i), 0, String.class), mh);
 	}
 
-	return mh;
-    }
+	private static MethodHandle createMHFromStrings2(String... strings)
+			throws Throwable {
+		var mh = dropArguments(constant(int.class, -1), 0, String.class);
+
+		for (int i = 0; i < strings.length; i++) {
+			mh = guardWithTest(insertArguments(STRING_EQUALS, 1, strings[i]),
+					dropArguments(constant(int.class, i), 0, String.class), mh);
+		}
+		return mh;
+	}
+
+	public static int stringSwitch2(String str) {
+		Objects.requireNonNull(str);
+		try {
+			var mh = createMHFromStrings2("foo", "bar", "bazz");
+			return (int) mh.invokeExact(str);
+		} catch (RuntimeException | Error e) {
+			throw e;
+		} catch (Throwable t) {
+			throw new UndeclaredThrowableException(t);
+		}
+	}
+
+	public static MethodHandle createMHFromStrings3(String... matches) {
+		return new InliningCache(matches).dynamicInvoker();
+	}
+
+	/* InlineCache => genere une suite d'instruction */
+	static class InliningCache extends MutableCallSite {
+		private static final MethodHandle SLOW_PATH;
+		static {
+			var lookup = MethodHandles.lookup();
+			try {
+				SLOW_PATH = lookup.findVirtual(InliningCache.class, "slowPath",
+						methodType(int.class, String.class));
+			} catch (NoSuchMethodException | IllegalAccessException e) {
+				throw new AssertionError(e);
+			}
+		}
+
+		private final List<String> matches;
+
+		public InliningCache(String... matches) {
+			super(methodType(int.class, String.class));
+			this.matches = List.of(matches);
+			setTarget(insertArguments(SLOW_PATH, 0, this));
+		}
+
+		private int slowPath(String value) {
+			var index = matches.indexOf(value);
+
+			var mh = guardWithTest(insertArguments(STRING_EQUALS, 1, value),
+					dropArguments(constant(int.class, index), 0, String.class),
+					getTarget());
+
+			setTarget(mh);
+			return index;
+		}
+	}
+
+	public static int stringSwitch3(String str) {
+		Objects.requireNonNull(str);
+		try {
+			var mh = createMHFromStrings3("foo", "bar", "bazz");
+			return (int) mh.invokeExact(str);
+		} catch (RuntimeException | Error e) {
+			throw e;
+		} catch (Throwable t) {
+			throw new UndeclaredThrowableException(t);
+		}
+	}
+
 }
