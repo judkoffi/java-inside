645ff7b6404046fa233e9d566b29dc671e6c6dda
Judicael KOFFI <judkoffi@gmail.com>
2019-10-29 09:56:48 +0100

    update travis

--- lab6/src/main/java/fr/umlv/java/inside/lab6/Example1.java
+++ lab6/src/main/java/fr/umlv/java/inside/lab6/Example1.java
@@ -3,10 +3,12 @@ package fr.umlv.java.inside.lab6;
 public class Example1 {
 	public static void main(String[] args) {
 		var scope = new ContinuationScope("hello1");
-		var continuation = new Continuation(scope,
-				() -> System.out.println("hello contiuation"));
+		var continuation = new Continuation(scope, () ->
+			{
+				Continuation.yield(scope);
+				System.out.println("hello contiuation");
+			});
 
-		continuation.yield(scope);
 		continuation.run();
 	}
 }
