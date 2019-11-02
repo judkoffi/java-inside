3ca06143558755b9e32a98b3fedd70af8e82d88c
judi <judkoffi@gmail.com>
2019-11-02 12:11:07 +0100

    implement eandom scheduling mode

--- lab6/src/main/java/fr/umlv/java/inside/lab6/Scheduler.java
+++ lab6/src/main/java/fr/umlv/java/inside/lab6/Scheduler.java
@@ -1,66 +1,73 @@
 package fr.umlv.java.inside.lab6;
 
+import java.util.AbstractCollection;
 import java.util.ArrayDeque;
-import java.util.Collections;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Scheduler {
 
-	public enum SCHEDULERMODE {
-		STACK, FIFO, RANDOM
-	}
+  public enum SCHEDULINGMODE {
+    STACK, FIFO, RANDOM
+  }
 
-	private final ArrayDeque<Continuation> queue;
-	private final SCHEDULERMODE mode;
+  private final AbstractCollection<Continuation> collection;
+  private final SCHEDULINGMODE mode;
 
-	public Scheduler(SCHEDULERMODE mode) {
-		this.queue = new ArrayDeque<Continuation>();
-		this.mode = mode;
-	}
 
-	public void enqueue(ContinuationScope scope) {
-		var continuation = Continuation.getCurrentContinuation(scope);
-		if (continuation == null) {
-			throw new IllegalStateException();
-		}
-		queue.offer(continuation);
-		Continuation.yield(scope);
-	}
+  public Scheduler(SCHEDULINGMODE mode) {
+    this.collection = (mode == SCHEDULINGMODE.RANDOM) ? new ArrayList<Continuation>()
+        : new ArrayDeque<Continuation>();
+    this.mode = mode;
+  }
 
-	private void stackRunLoop() {
-		while (!queue.isEmpty()) {
-			var continuation = queue.pollLast();
-			continuation.run();
-		}
-	}
+  public void enqueue(ContinuationScope scope) {
+    var continuation = Continuation.getCurrentContinuation(scope);
+    if (continuation == null) {
+      throw new IllegalStateException();
+    }
 
-	private void fifoRunLoop() {
-		while (!queue.isEmpty()) {
-			var continuation = queue.pollFirst();
-			continuation.run();
-		}
-	}
+    collection.add(continuation);
+    // ((ArrayDeque<Continuation>) collection).offer(continuation);
 
-	private void randomkRunLoop() {
-		while (!queue.isEmpty()) {
-			var shuffleQueue = queue.clone();
-			Collections.shuffle(List.of(shuffleQueue));
-			var continuation = shuffleQueue.getFirst();
-			continuation.run();
-		}
-	}
+    Continuation.yield(scope);
+  }
 
-	public void runLoop() {
-		switch (mode) {
-			case STACK :
-				stackRunLoop();
-				break;
-			case FIFO :
-				fifoRunLoop();
-				break;
-			case RANDOM :
-				randomkRunLoop();
-				break;
-		}
-	}
+  private void stackRunLoop() {
+    while (!collection.isEmpty()) {
+      var continuation = ((ArrayDeque<Continuation>) collection).pollLast();
+      continuation.run();
+    }
+  }
+
+  private void fifoRunLoop() {
+    while (!collection.isEmpty()) {
+      var continuation = ((ArrayDeque<Continuation>) collection).pollFirst();
+      continuation.run();
+    }
+  }
+
+  private void randomRunLoop() {
+    while (!collection.isEmpty()) {
+      var randomIndex = ThreadLocalRandom.current().nextInt(0, collection.size());
+      var continuation = ((ArrayList<Continuation>) collection).get(randomIndex);
+
+      if (!continuation.isDone())
+        continuation.run();
+    }
+  }
+
+  public void runLoop() {
+    switch (mode) {
+      case STACK:
+        stackRunLoop();
+        break;
+      case FIFO:
+        fifoRunLoop();
+        break;
+      case RANDOM:
+        randomRunLoop();
+        break;
+    }
+  }
 }
--- lab6/src/main/java/fr/umlv/java/inside/lab6/SchedulerExample.java
+++ lab6/src/main/java/fr/umlv/java/inside/lab6/SchedulerExample.java
@@ -1,53 +1,53 @@
 package fr.umlv.java.inside.lab6;
 
 import java.util.List;
-
-import fr.umlv.java.inside.lab6.Scheduler.SCHEDULERMODE;
+import fr.umlv.java.inside.lab6.Scheduler.SCHEDULINGMODE;
 
 public class SchedulerExample {
-	public static void main(String[] args) {
 
-		var scheduler = new Scheduler(SCHEDULERMODE.STACK);
+  public static void main(String[] args) {
+    var scheduler = new Scheduler(SCHEDULINGMODE.RANDOM);
+    var scope = new ContinuationScope("scope");
 
-		var scope = new ContinuationScope("scope");
-		var continuation1 = new Continuation(scope, () ->
-			{
-				System.out.println("start 1");
-				scheduler.enqueue(scope);
-				System.out.println("middle 1");
-				scheduler.enqueue(scope);
-				System.out.println("end 1");
-			});
-		var continuation2 = new Continuation(scope, () ->
-			{
-				System.out.println("start 2");
-				scheduler.enqueue(scope);
-				System.out.println("middle 2");
-				scheduler.enqueue(scope);
-				System.out.println("end 2");
-			});
+    var continuation1 = new Continuation(scope, () ->
+    {
+      System.out.println("start 1");
+      scheduler.enqueue(scope);
+      System.out.println("middle 1");
+      scheduler.enqueue(scope);
+      System.out.println("end 1");
+    });
 
-		var continuation3 = new Continuation(scope, () ->
-			{
-				System.out.println("start 3");
-				scheduler.enqueue(scope);
-				System.out.println("middle 3");
-				scheduler.enqueue(scope);
-				System.out.println("end 3");
-			});
+    var continuation2 = new Continuation(scope, () ->
+    {
+      System.out.println("start 2");
+      scheduler.enqueue(scope);
+      System.out.println("middle 2");
+      scheduler.enqueue(scope);
+      System.out.println("end 2");
+    });
 
-		var continuation4 = new Continuation(scope, () ->
-			{
-				System.out.println("start 4");
-				scheduler.enqueue(scope);
-				System.out.println("middle 4");
-				scheduler.enqueue(scope);
-				System.out.println("end 4");
-			});
+    var continuation3 = new Continuation(scope, () ->
+    {
+      System.out.println("start 3");
+      scheduler.enqueue(scope);
+      System.out.println("middle 3");
+      scheduler.enqueue(scope);
+      System.out.println("end 3");
+    });
 
-		var list = List.of(continuation1, continuation2, continuation3,
-				continuation4);
-		list.forEach(Continuation::run);
-		scheduler.runLoop();
-	}
+    var continuation4 = new Continuation(scope, () ->
+    {
+      System.out.println("start 4");
+      scheduler.enqueue(scope);
+      System.out.println("middle 4");
+      scheduler.enqueue(scope);
+      System.out.println("end 4");
+    });
+
+    var list = List.of(continuation1, continuation2, continuation3, continuation4);
+    list.forEach(Continuation::run);
+    scheduler.runLoop();
+  }
+
 }
