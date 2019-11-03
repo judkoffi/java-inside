b4ab8b36ce633a69555970d04815add4cac35a5c
judi <judkoffi@gmail.com>
2019-11-03 18:08:57 +0100

    add macos jdk14 download

--- script.sh
+++ script.sh
@@ -2,8 +2,13 @@
 set -ev
 
 if [ "$FOLDER" = "lab6" ]; then
-    wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-linux.tar.gz
-    tar xvf jdk-14-loom-linux.tar.gz
+    if [ "$OSTYPE" = "darwin17" ]; then # OS = osx
+        wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-osx.tar.gz
+        tar xvf jdk-14-loom-osx.tar.gz
+    else
+        wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-linux.tar.gz
+        tar xvf jdk-14-loom-linux.tar.gz
+    fi
     export JAVA_HOME=$(pwd)/jdk-14-loom/
 fi
 
